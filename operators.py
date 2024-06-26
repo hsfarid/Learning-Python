462f162ead9c6e9e8c1b343b722092c130b2a20b
Farid Habib <hsfarid08@gmail.com>
2024-06-26 00:02:10 +0000

    add operators.py

---
+++ operators.py
@@ -0,0 +1,44 @@
+# Python divides the operators in the following groups:
+# Arithmetic operators
+# Assignment operators
+# Comparison operators
+# Logical operators
+# Identity operators
+# Membership operators
+# Bitwise operators
+
+# Python Arithmetic Operators
+# Arithmetic operators are used with numeric values to perform common mathematical operations:
+
+# x = 6
+# y = 4
+# print(x + y)
+# print(x * y)
+# print(x / y)
+# print(x % y)
+# print(x - y)
+# print(x ** y)
+# print(x // y) 
+
+# Python Assignment Operators
+# Assignment operators are used to assign values to variables:
+# x = 4
+# x += 3
+# x -= 3
+# x *= 3
+# x /= 3
+# x %= 3
+# x //= 3
+
+# Python Comparison Operators
+# Comparison operators are used to compare two values:
+x = 4
+y = 5
+print(x == y)
+print(x < y)
+print(x <= y)
+print(x != y)
+print(x > y)
+print(x >= y)
+
+
