8c5601a9d95710d3198eddc8aaa3d1b9c7d64d97
= <=>
2024-06-29 01:31:24 +0000

    add forloop.py

---
+++ forLoop.py
@@ -0,0 +1,21 @@
+# iterating over lists
+fruits = ["banana", "grapes", "apple","cherry"]
+for fruit in fruits:
+    print(fruit)
+
+for i in range(len(fruits)):
+    print(fruits[i])
+
+# iterating over tuples
+colors = ("red","green", "blue")
+for color in colors:
+    print(color)
+
+# looping through a character in a string
+message = "Hello world"
+for character in message:
+    print(character)
+
+# iterating over ranges
+for x in range(1, 10):
+    print(x)
