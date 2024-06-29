2d8fa237c13be8bdb843e6314153e7570a3964f7
= <=>
2024-06-29 00:27:02 +0000

    add operators.py

---
+++ operators.py
@@ -0,0 +1,26 @@
+# Python Operators
+# Operators are used to perform operations on variables and values.
+
+# In the example below, we use the + operator to add together two values:
+print(10 + 7)
+
+# Python divides the operators in the following groups:
+# Arithmetic operators
+# Assignment operators
+# Comparison operators
+# Logical operators
+# Identity operators
+# Membership operators
+# Bitwise operators
+
+# Python Arithmetic Operators
+# Arithmetic operators are used with numeric values to perform common mathematical operations:
+a = 10
+b = 5
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
+print( a // b)
