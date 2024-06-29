b4ef77f7f0dc095f70c045fd327910fefa17204b
Farid Habib <hsfarid08@gmail.com>
2024-06-29 13:10:06 +0000

    add while.py

---
+++ while.py
@@ -0,0 +1,11 @@
+# i = 0
+# while i <= 5:
+#     print('Hello', i)
+#     i += 1
+
+age = 0
+
+while age < 18:
+    age = int(input("Enter your age: "))
+    
+print("You are old enough to proceed.")
