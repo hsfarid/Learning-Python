1e99bec03d8e52aa7d8cd2f65f888ec4a22e6fdb
= <=>
2024-07-02 04:22:07 +0000

    get weather information using the requests library

---
+++ weather.py
@@ -0,0 +1,13 @@
+import requests
+import datetime as dt
+
+apiKey = "6e855adb181d00a009ab8a5c19e99326"
+baseURL = "https://api.openweathermap.org/data/2.5/weather?q="
+cityName = input("Enter city name: ")
+
+completeURL = baseURL  + cityName + "&appid=" + apiKey
+response = requests.get(completeURL)
+data = response.json()
+print("Current temperature: ", data["main"]["temp"])
+print("Current temperature: ", data["main"]["temp"])
+print("Current temperature: ", data["main"]["temp"])
