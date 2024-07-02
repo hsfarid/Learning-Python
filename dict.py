f646f4b2656f62c24e61693855b94d3bacc51d7d
= <=>
2024-07-02 02:46:50 +0000

    add dict.py

---
+++ dict.py
@@ -0,0 +1,10 @@
+user = {"name": "Fareed", "age": 27, "email": "hsfarid08@gmail.com"}
+#accessing items
+print(user["name"])
+print(user.get("age"))
+#see all keys in the dictionary
+print(user.keys())
+#see all values in the dictionary
+print(user.values())
+#see all items in the dictionary in a tuple
+print(user.items())
