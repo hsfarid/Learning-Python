b8c5851618c82dc8bba800ae11f42a074983a933
= <=>
2024-07-02 02:11:10 +0000

    add tuples.py

---
+++ tuples.py
@@ -0,0 +1,11 @@
+student = ("reed", 21, "male")
+#indexing
+print(student[1])
+#count
+print(student.count("reed"))
+#find index of a value
+print(student.index('reed'))
+
+#looping through tuples
+for x in student:
+    print(x)
